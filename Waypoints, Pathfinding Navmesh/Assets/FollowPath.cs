142f1e4e2aacff87a80da15584c0f6eee038d5dc
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-04-02 18:49:46 -0300

    WPManager e FollowPath com comentarios

---
+++ Waypoints, Pathfinding Navmesh/Assets/FollowPath.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    //variavel com o transform do objetivo
+    Transform goal;
+    //velocidade de locomoção do gameobject
+    float speed = 5.0f;
+    // variavel para calcular a distancia para o ponto
+    float accuracy = 1.0f;
+    //variavel da velocidade da rotação
+    float rotSpeed = 2.0f;
+    //varia da instancia do wpManager
+    public GameObject wpManager;
+    //pontos que o gameobject ira passar
+    GameObject[] wps;
+    // nó atual
+    GameObject currentNode;
+    //criando e definindo o ponto atual como 0
+    int currentWP = 0;
+    //varia da instancia do Graph
+    Graph g;
+    void Start()
+    {
+        //pegando as variaveis das intancias 
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        //definindo o nó atual como 0
+        currentNode = wps[0];
+    }
+
+    // metodo que vai para o botão fazer o tank ir ao helicoptero
+    public void GoToHeli()
+    {
+        //pega o metodo do graph, onde é passado o ponto atual e o ponto final
+        g.AStar(currentNode, wps[9]);
+        //ao terminar, define o ponto como 0
+        currentWP = 0;
+    }
+
+    //  metodo que vai para o botão fazer o tank ir ao helicoptero ruinas 
+    public void GoToRuin()
+    {
+        //pega o metodo do graph, onde é passado o ponto atual e o ponto final
+        g.AStar(currentNode, wps[3]);
+        //ao terminar, define o ponto como 0
+        currentWP = 0;
+    }
+
+void LateUpdate()
+    {
+        //se o caminho tiver finalizado, ele é zerado
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+          return;
+
+        //o nó atual é igual ao ponto atual
+        currentNode = g.getPathPoint(currentWP);
+
+        //se a distancia do ponto atual para o proximo for menor que o valor setado em accuracy, ele soma mais um no ponto atual
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position,transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        //se o ponto atual for menor que o caminho
+        if (currentWP < g.getPathLength())
+        {
+            //define o proximo ponto do caminho
+            goal = g.getPathPoint(currentWP).transform;
+            //ve aonde esta o proximo ponto
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
+            //faz a rotação para o proximo ponto
+            Vector3 direction = lookAtGoal - this.transform.position; this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
+        }
+        //realiza a movimentação do tank ao ponto
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ Waypoints, Pathfinding Navmesh/Assets/WPManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct Link
+{
+    public enum direction { UNI, BI }
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    //Pontos que vai percorrer
+    public GameObject[] waypoints;
+
+    //Links dos pontos
+    public Link[] links;
+
+    //Instancia do Graph
+    public Graph graph = new Graph();
+
+
+    void Start()
+    {
+        //Verificação se o array nao está vazio
+        if (waypoints.Length > 0)
+        {
+            //vai localizar quantos objetos tem no array
+            foreach (GameObject wp in waypoints)
+            {
+                //e adiciona um nó pra cada objeto
+                graph.AddNode(wp);
+            }
+
+            //vai localizar quantos objetos tem no array
+            foreach (Link l in links)
+            {
+                //utiliza o metodo do graph para adicionar um ed
+                graph.AddEdge(l.node1, l.node2);
+                //verifica se a direção é igual BI
+                if (l.dir == Link.direction.BI)
+                    //utiliza o metodo do graph para adicionar um edge
+                    graph.AddEdge(l.node2, l.node1);
+            }
+        }
+    }
+    void Update()
+    {
+        graph.debugDraw();
+    }
+}
+
